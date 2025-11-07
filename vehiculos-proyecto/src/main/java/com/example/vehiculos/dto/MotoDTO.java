3935348874f10062c726ba69332bd49b65e251fe
Juan Esteban Ospina Pinzon <juan.ospinanz@amigo.edu.co>
2025-11-06 23:28:05 -0500

    Delete vehiculos-proyecto directory

--- vehiculos-proyecto/src/main/java/com/example/vehiculos/dto/MotoDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.vehiculos.dto;
-
-public class MotoDTO extends VehiculoDTO {
-    public MotoDTO() {}
-    public MotoDTO(Long id, String marca, String modelo, Integer anio, String imagenUrl) {
-        super(id, marca, modelo, anio, imagenUrl);
-    }
-}
-
